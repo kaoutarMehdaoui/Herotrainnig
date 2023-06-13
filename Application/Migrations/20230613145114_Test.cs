98293c1a57422ee1c14c98d8114b54420c14418c
Kaoutar Mehdaoui <kmehdaoui@smb.sqli.com>
2023-06-13 15:58:20 +0100

    second commit

---
+++ Application/Migrations/20230613145114_Test.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Heros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    NameHero = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    photo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    gender = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heros", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Powers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Powers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeroPower",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HerosId = table.Column<int>(type: "int", nullable: false),
+                    PowersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroPower", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HeroPower_Heros_HerosId",
+                        column: x => x.HerosId,
+                        principalTable: "Heros",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_HeroPower_Powers_PowersId",
+                        column: x => x.PowersId,
+                        principalTable: "Powers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeroPower_HerosId",
+                table: "HeroPower",
+                column: "HerosId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeroPower_PowersId",
+                table: "HeroPower",
+                column: "PowersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HeroPower");
+
+            migrationBuilder.DropTable(
+                name: "Heros");
+
+            migrationBuilder.DropTable(
+                name: "Powers");
+        }
+    }
+}
